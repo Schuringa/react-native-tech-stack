dc75a1072e1b3fd78e8d9c93692056b58d1f395d
jordans <schuringa.jordan@gmail.com>
2017-07-18 12:48:50 +1000

    Added library list to app. Create function for mapping state to prop for library component.

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { connect } from 'react-redux'
 
 class LibraryList extends Component {
   render () {
-    return <View />
+    return console.log(this.props)
   }
 }
 
-export default LibraryList
+const mapStateToProps = state => {
+  return {
+    libraries: state.libraries
+  }
+}
+
+export default connect(mapStateToProps)(LibraryList)
